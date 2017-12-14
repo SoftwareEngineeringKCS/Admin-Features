aa6c00433b72d1ac68ad43505b6eb1960523e470
Jorge Espada <32748368+espadajo@users.noreply.github.com>
2017-12-13 20:04:20 -0500

    Update load_stats.php

--- load_stats.php
+++ load_stats.php
@@ -5,83 +5,89 @@
 
 	$check_data = FALSE;
 
-	$inStart = $_REQUEST['inStart'];
-	$inEnd = $_REQUEST['inEnd'];
-	$inWhatVal = (int)$_REQUEST['inWhatVal'];
-	$inWhatText = $_REQUEST['inWhatText'];
-	$inByVal = (int)$_REQUEST['inByVal'];
-	$inByText = $_REQUEST['inByText'];
-	if ($_REQUEST['inAll'] == 'false') {
-		$inAll = 0;
+	if (!isset($_REQUEST['inStart'])) {
+		echo "<h1>Forbidden</h1>";
+		echo "<p style='font-size: 1.2em'>You don't have permission to access this page.</p>";
 	} else {
-		$inAll = 1;
-	}
-	$inConsultantId = $_REQUEST['inConsultantId'];
-
-	# Validate data.
-	$vInStart = date_parse($inStart);
-	$vInEnd = date_parse($inEnd);
-	if (checkdate($vInStart['month'], $vInStart['day'], $vInStart['year']) && checkdate($vInEnd['month'], $vInEnd['day'], $vInEnd['year']) && $inWhatVal > 0 && $inByVal > 0) {
-		
-		echo "<p class='result' style='text-align: center'>";
-		if ($inAll == 0) {
-			$query = sprintf("SELECT CONCAT(last_name, ', ', first_name) FROM Consultants WHERE id = '%s'", $inConsultantId);
-			$stats_res1 = mysqli_query($conex, $query);
-			if ($stats_res1) {
-				if (mysqli_num_rows($stats_res1) > 0) {
-					$r = mysqli_fetch_array($stats_res1);
-					echo "<b>Consultants</b>: " . $r[0] . " | ";
-				} else {
-					echo "<b>Consultants</b>: Current | ";		
-				}
-
-				mysqli_free_result($stats_res1);
-			} else {
-				echo "<b>Consultants</b>: Current | ";	
-			}
+		$inStart = $_REQUEST['inStart'];
+		$inEnd = $_REQUEST['inEnd'];
+		$inWhatVal = (int)$_REQUEST['inWhatVal'];
+		$inWhatText = $_REQUEST['inWhatText'];
+		$inByVal = (int)$_REQUEST['inByVal'];
+		$inByText = $_REQUEST['inByText'];
+		if ($_REQUEST['inAll'] == 'false') {
+			$inAll = 0;
 		} else {
-			echo "<b>Consultants</b>: All | ";
+			$inAll = 1;
 		}
-		echo "<b>Range</b>: " . date_format(date_create($inStart), 'M j, Y') . " - " . date_format(date_create($inEnd), 'M j, Y');
-		echo "<br><b>What you got</b>: " . $inWhatText . " | ";
-		echo "<b>Filtered by</b>: " . $inByText;
-		echo "</p>";
+		$inConsultantId = $_REQUEST['inConsultantId'];
 
-		$query = sprintf("CALL usp_Load_Stats('%d', '%s', '%s', '%s', '%d', '%d')", $inAll, $inConsultantId, $inStart, $inEnd, $inWhatVal, $inByVal);
-		$stats_res2 = mysqli_query($conex, $query);
-		if ($stats_res2) {
-			$labels = array();
-			$data = array();
-			$bground = array();
-			$bcolor = array();
-			while ($row = mysqli_fetch_assoc($stats_res2)) {
-				for ($i=0; $i < (count($row)-1) ; $i++) { 
-		 			array_push($labels, $row['label']);
-		 			array_push($data, $row['num']);
-		 			array_push($bground, 'rgba(54, 162, 235, 0.2)');
-		 			array_push($bcolor, 'rgba(54, 162, 235, 1)');
-				}
-			}
+		# Validate data.
+		$vInStart = date_parse($inStart);
+		$vInEnd = date_parse($inEnd);
+		if (checkdate($vInStart['month'], $vInStart['day'], $vInStart['year']) && checkdate($vInEnd['month'], $vInEnd['day'], $vInEnd['year']) && $inWhatVal > 0 && $inByVal > 0) {
 			
-			$check_data = TRUE;
-			mysqli_free_result($stats_res2);
+			echo "<p class='result' style='text-align: center; padding: 0px 0px 0px 0px;'>";
+			if ($inAll == 0) {
+				$query = sprintf("SELECT CONCAT(last_name, ', ', first_name) FROM Consultants WHERE id = '%s'", $inConsultantId);
+				$stats_res1 = mysqli_query($conex, $query);
+				if ($stats_res1) {
+					if (mysqli_num_rows($stats_res1) > 0) {
+						$r = mysqli_fetch_array($stats_res1);
+						echo "<b>Consultants</b>: " . $r[0] . " | ";
+					} else {
+						echo "<b>Consultants</b>: Current | ";		
+					}
+
+					mysqli_free_result($stats_res1);
+				} else {
+					echo "<b>Consultants</b>: Current | ";	
+				}
+			} else {
+				echo "<b>Consultants</b>: All | ";
+			}
+			echo "<b>Range</b>: " . date_format(date_create($inStart), 'M j, Y') . " - " . date_format(date_create($inEnd), 'M j, Y');
+			echo "<br><b>What you got</b>: " . $inWhatText . " | ";
+			echo "<b>Filtered by</b>: " . $inByText;
+			echo "</p>";
+
+			$query = sprintf("CALL usp_Load_Stats('%d', '%s', '%s', '%s', '%d', '%d')", $inAll, $inConsultantId, $inStart, $inEnd, $inWhatVal, $inByVal);
+			$stats_res2 = mysqli_query($conex, $query);
+			if ($stats_res2) {
+				$labels = array();
+				$data = array();
+				$bground = array();
+				$bcolor = array();
+				while ($row = mysqli_fetch_assoc($stats_res2)) {
+					for ($i=0; $i < (count($row)-1) ; $i++) { 
+			 			array_push($labels, $row['label']);
+			 			array_push($data, $row['num']);
+			 			array_push($bground, 'rgba(54, 162, 235, 0.2)');
+			 			array_push($bcolor, 'rgba(54, 162, 235, 1)');
+					}
+				}
+				
+				$check_data = TRUE;
+				mysqli_free_result($stats_res2);
+			} else {
+				echo "<p class='error'>Loading Data... Failed! [Connection Error]";
+				echo "<br>Contact Administrator!</p>";
+				echo "<p><a href='staff.php'>TRY AGAIN</a></p>";
+			}
+
 		} else {
-			echo "<p class='error'>Loading Data... Failed! [Connection Error]";
-			echo "<br>Contact Administrator!</p>";
-			echo "<p><a href='staff.php'>TRY AGAIN</a></p>";
+			echo "<h2>The following fields are invalid!</h2>";
+			echo "<p class='error'>";
+			if (!checkdate($vInStart['month'], $vInStart['day'], $vInStart['year'])) echo "\"Start Date\", ";
+			if (!checkdate($vInEnd['month'], $vInEnd['day'], $vInEnd['year'])) echo "\"End Date\", ";
+			if (!$inWhatVal > 0) echo "\"What you get\", ";
+			if (!$inByVal > 0) echo "\"Filter by\"";
+			echo "</p>";
 		}
 
-	} else {
-		echo "<h2>The following fields are invalid!</h2>";
-		echo "<p class='error'>";
-		if (!checkdate($vInStart['month'], $vInStart['day'], $vInStart['year'])) echo "\"Start Date\", ";
-		if (!checkdate($vInEnd['month'], $vInEnd['day'], $vInEnd['year'])) echo "\"End Date\", ";
-		if (!$inWhatVal > 0) echo "\"What you get\", ";
-		if (!$inByVal > 0) echo "\"Filter by\"";
-		echo "</p>";
+		mysqli_close($conex);
 	}
 
-	mysqli_close($conex);
 ?>
 
 <br>
